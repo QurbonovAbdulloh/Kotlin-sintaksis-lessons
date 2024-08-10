1415139036017d6a71357e30d1167d54cdf2900c
Khurbanov_Dev <abdullohqurbonov5446@gmail.com>
2024-08-10 22:12:59 +0500

    second commit

---
+++ src/Lesson6/Main.kt
@@ -0,0 +1,105 @@
+package Lesson6
+
+fun main() {
+
+   /* for (i in 1..100 step 2) {
+//        print(i)
+    }
+
+    val array = arrayOf("Nimadur", "Kimdur", "Yana nima")
+    for (element in array) {
+//        println(element)
+    }
+
+    for (i in array.indices) {
+//        println(array[i])
+    }
+
+    for (i in 5 downTo 1) {
+//        print(i)
+    }
+
+    var language = arrayOf("Ruby", "Kotlin", "Python", "Java")
+//    for (item in language) println(item)
+    for (item in language.indices) {
+        if (item % 2 == 0) {
+//            println(language[item])
+        }
+
+    }
+    val text = "Kotlin"
+    for (letter in text) {
+//            print(letter)
+    }
+
+    for (item in text.indices) {
+//        print(text[item])
+    }
+
+    for (index in language.withIndex()) {
+//        println("$index")
+    }
+
+   *//* var i = 0
+    while (i < 5) {
+//        println(i)
+        i++
+    }*//*
+
+    var b = 1
+    while (b <= 5) {
+//        println("Line $b")
+//        i++
+        break
+    }
+
+    var v = 2
+    while (v < 5) {
+//        println("Line $v")
+    }
+*/
+  /*  var sum = 0
+    var i = 100
+    while (i != 0) {
+        sum += i
+        --i
+    }
+    println("sum = $sum")*/
+
+    /*while (true) {
+        println("loop")
+    }*/
+
+//    println("Something")
+
+   /* var num = 100
+    do {
+        println("Loop: $num")
+        num++
+    } while (num <= 105)*/
+
+/*    for (n in 1..5) {
+        println("before continue, Loop: $n")
+        if (n == 2 || n == 4)
+            continue
+            println("After continue, Loop: $n")
+
+    }*/
+
+    /*for (n in 1..10) {
+        if (n %2 != 0)
+            continue
+            println("$n")
+
+    }*/
+
+    /*for (x in 'A'..'D') {
+        for (n in 1..4) {
+            if (n == 2 || n == 4)
+                continue
+            println("$x and $n")
+        }
+    }*/
+
+}
+
